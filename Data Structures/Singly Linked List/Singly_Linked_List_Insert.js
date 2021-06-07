99c29ba149bdbbfccd9ee91098ae7bf2723eca51
bhushanjain08 <bhushanjain81097@gmail.com>
2021-06-07 13:24:51 +0530

    notes

--- Data Structures/Singly Linked List/Singly_Linked_List_Insert.js
+++ Data Structures/Singly Linked List/Singly_Linked_List_Insert.js
@@ -1,19 +1,19 @@
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
     }
 }
 
-class SinglyLinkedList{
-    constructor(){
+class SinglyLinkedList {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val){
+    push(val) {
         var newNode = new Node(val);
-        if(!this.head){
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
@@ -23,36 +23,36 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
-    pop(){
-        if(!this.head) return undefined;
+    pop() {
+        if (!this.head) return undefined;
         var current = this.head;
         var newTail = current;
-        while(current.next){
+        while (current.next) {
             newTail = current;
             current = current.next;
         }
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
-        if(this.length === 0){
+        if (this.length === 0) {
             this.head = null;
             this.tail = null;
         }
         return current;
     }
-    shift(){
-        if(!this.head) return undefined;
+    shift() {
+        if (!this.head) return undefined;
         var currentHead = this.head;
         this.head = currentHead.next;
         this.length--;
-        if(this.length === 0){
+        if (this.length === 0) {
             this.tail = null;
         }
         return currentHead;
     }
-    unshift(val){
+    unshift(val) {
         var newNode = new Node(val);
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         }
@@ -61,29 +61,29 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
-    get(index){
-        if(index < 0 || index >= this.length) return null;
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
         var counter = 0;
         var current = this.head;
-        while(counter !== index){
+        while (counter !== index) {
             current = current.next;
             counter++;
         }
         return current;
     }
-    set(index, val){
+    set(index, val) {
         var foundNode = this.get(index);
-        if(foundNode){
+        if (foundNode) {
             foundNode.val = val;
             return true;
         }
         return false;
     }
-    insert(index, val){
-        if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);
-        if(index === 0) return !!this.unshift(val);
-        
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val); // !! gives the boolean trure/false
+        if (index === 0) return !!this.unshift(val);
+
         var newNode = new Node(val);
         var prev = this.get(index - 1);
         var temp = prev.next;
@@ -100,5 +100,3 @@ list.push(100)
 list.push(201)
 list.push(250)
 list.push(350)
-
-
--- Data Structures/Singly Linked List/Singly_Linked_List_Reverse.js
+++ Data Structures/Singly Linked List/Singly_Linked_List_Reverse.js
@@ -1,19 +1,19 @@
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
     }
 }
 
-class SinglyLinkedList{
-    constructor(){
+class SinglyLinkedList {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val){
+    push(val) {
         var newNode = new Node(val);
-        if(!this.head){
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
@@ -23,36 +23,36 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
-    pop(){
-        if(!this.head) return undefined;
+    pop() {
+        if (!this.head) return undefined;
         var current = this.head;
         var newTail = current;
-        while(current.next){
+        while (current.next) {
             newTail = current;
             current = current.next;
         }
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
-        if(this.length === 0){
+        if (this.length === 0) {
             this.head = null;
             this.tail = null;
         }
         return current;
     }
-    shift(){
-        if(!this.head) return undefined;
+    shift() {
+        if (!this.head) return undefined;
         var currentHead = this.head;
         this.head = currentHead.next;
         this.length--;
-        if(this.length === 0){
+        if (this.length === 0) {
             this.tail = null;
         }
         return currentHead;
     }
-    unshift(val){
+    unshift(val) {
         var newNode = new Node(val);
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         }
@@ -61,29 +61,29 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
-    get(index){
-        if(index < 0 || index >= this.length) return null;
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
         var counter = 0;
         var current = this.head;
-        while(counter !== index){
+        while (counter !== index) {
             current = current.next;
             counter++;
         }
         return current;
     }
-    set(index, val){
+    set(index, val) {
         var foundNode = this.get(index);
-        if(foundNode){
+        if (foundNode) {
             foundNode.val = val;
             return true;
         }
         return false;
     }
-    insert(index, val){
-        if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);
-        if(index === 0) return !!this.unshift(val);
-        
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val);
+        if (index === 0) return !!this.unshift(val);
+
         var newNode = new Node(val);
         var prev = this.get(index - 1);
         var temp = prev.next;
@@ -92,34 +92,34 @@ class SinglyLinkedList{
         this.length++;
         return true;
     }
-    remove(index){
-        if(index < 0 || index >= this.length) return undefined;
-        if(index === 0) return this.shift();
-        if(index === this.length - 1) return this.pop();
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
         var previousNode = this.get(index - 1);
         var removed = previousNode.next;
         previousNode.next = removed.next;
         this.length--;
         return removed;
     }
-    reverse(){
-      var node = this.head;
-      this.head = this.tail;
-      this.tail = node;
-      var next;
-      var prev = null;
-      for(var i = 0; i < this.length; i++){
-        next = node.next;
-        node.next = prev;
-        prev = node;
-        node = next;
-      }
-      return this;
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+        for (var i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
     }
-    print(){
+    print() { // only use to print the list in array format, only for understanding 
         var arr = [];
         var current = this.head
-        while(current){
+        while (current) {
             arr.push(current.val)
             current = current.next
         }
@@ -134,11 +134,3 @@ list.push(201)
 list.push(250)
 list.push(350)
 list.push(999)
-
-
-
-
-
-
-
-
